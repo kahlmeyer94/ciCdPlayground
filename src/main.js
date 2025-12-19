9bebecfc6ba68b4c75dda44dbfc8ed9cae9ff518
Paul Kahlmeyer <paul.kahlmeyer@tngtech.com>
2025-12-19 13:56:06 +0100

    preparation

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Paul Kahlmeyer",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Automation"],
     // Which CI/CD tools do you use in your project?
     tools: "Jenkins, maybe?",
     // What do you want to learn in this workshop?
